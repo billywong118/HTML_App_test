ca4a8bb26c7430b06fb1c7843b2adc3002fdf53e
Billy Wong <billywong118@gmail.com>
2015-07-27 13:12:06 -0400

    even more changes to gps

--- js/add-marker.js
+++ js/add-marker.js
@@ -14,11 +14,11 @@ function initialize2() {
 
 // Adds a marker to the map.
 function addMarker(location) {
-	var image = 'exclamation.png';
+	var image = 'pink_MarkerO.png';
   var marker = new google.maps.Marker({
     position: location,
     map: map,
-	icon: image,
+	'icon': {'url':image},
 	/*label: prompt('Enter name for this location'),*/
   });
   /*marker.myID = prompt('Enter name for this location');*/
