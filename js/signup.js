44bc5fea2ad055c929224c293d2d2728a03189de
Billy Wong <billywong118@gmail.com>
2015-07-29 17:47:53 -0400

    modified parse for sign up

--- js/signup.js
+++ js/signup.js
@@ -2,6 +2,8 @@ Parse.initialize('M1ifyaGlqMi0qLtPDMcdr5vl75mCxon6FizGfEdJ','UOnRWMWcLfmFC8fsD0I
 var UserObject = Parse.Object.extend("Users");
 var userObject = new UserObject();
 
+
+function create() {
 /*var first_name = $("#first_name").val();
 var last_name = $("#last_name").val();
 var email = $("#email").val();
@@ -10,7 +12,7 @@ var user = $("#user").val();
 var password = $("#password").val();
 var password2 = $("#password2").val();*/
 
-var first_name = document.getElementById("first_name").value);
+var first_name = document.getElementById("first_name").value;
 var last_name = document.getElementById("last_name").value;
 var email = document.getElementById("email").value;
 var email2 = document.getElementById("email2").value;
@@ -18,12 +20,12 @@ var user = document.getElementById("user").value;
 var password = document.getElementById("password").value;
 var password2 = document.getElementById("password2").value;
 
-document.getElementById().value
+/*document.getElementById().value*/
 
 
 userObject.set("first_name", first_name);
 userObject.set("last_name", last_name);
-userObject.set("email", email1);
+userObject.set("email", email);
 userObject.set("user", user);
 userObject.set("password", password);
 
@@ -38,6 +40,7 @@ userObject.save(null, {
     alert('Failed to create new object, with error code: ' + error.message);
   }
 });
+}
 
 
 	
