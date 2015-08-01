0fd7b1c0fad59342ed7725a68b467144a02bac16
Billy Wong <billywong118@gmail.com>
2015-07-31 21:33:05 -0400

    testing alerts

--- js/buttons.js
+++ js/buttons.js
@@ -9,6 +9,55 @@ function addbalance() {
 	updateBalance();
 }
 
+function subtractbalance_vtwo() {
+	var amount = document.getElementById("money_to_subtract").value;
+	var balance = document.getElementById("balance");
+	var next_balance = ( Number(balance.innerHTML.substring(1)) - Number(amount) ).toFixed(2);
+	balance.innerHTML = '$' + next_balance.toString();
+	updateBalance_versiontwo(amount);
+}
+
+
+
+function updateBalance_versiontwo(amount) {
+	/*have to retrieve the value for respective category then add the value subtracted*/
+	var category;
+	var email = window.localStorage.getItem("User");
+	var radios = document.getElementsByName('category');
+
+	for (var i = 0, length = radios.length; i < length; i++) {
+		if (radios[i].checked) {
+			// do whatever you want with the checked radio
+			category = radios[i].value;
+
+			// only one radio can be logically checked, don't check the rest
+			break;
+		}
+	}
+	var balance = Number(document.getElementById("balance").innerHTML.substring(1));
+	var object;
+	var query = new Parse.Query(UserObject);
+	query.find({
+	success: function(results) {
+		for (var i = 0; i < results.length; i++) {
+			if (results[i].get("username") === email) {
+			object = results[i];}		
+		}
+		object.set('balance',balance);
+		object.save(null, {
+		success: function(object) {
+		alert(category);
+		alert('Balance updated!');
+  }
+});
+	},
+	error: function(error) {
+		alert("Error: " + error.code + " " + error.message);
+	}
+	})
+}
+
+
 function subtractbalance() {
 	var amount = prompt("Specify amount to be subtracted");
 	while (isNaN(amount)) {		
@@ -22,7 +71,7 @@ function subtractbalance() {
 
 /*work on where the negative sign is */
 
-function updateBalance(new_balance) {
+function updateBalance() {
 	
 	var email = window.localStorage.getItem("User");
 
